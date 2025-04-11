f7afcb8af9c021a83d4350f07ef9ad1fb4f1c2bc
Valentin Condurache <85354451+ValentinCondurache@users.noreply.github.com>
2025-04-11 13:24:03 +0300

    Test deploy

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ const Home = () => {
         <h2 className={stylesHome.title}>Dashboard</h2>
         <p className={stylesHome['welcome-text']}>
           Welcome, {user?.metadata?.firstName || 'stranger'}{' '}
-          <span role="img" alt="hello">
+          <span role="img" alt="hello-message">
             👋
           </span>
         </p>
